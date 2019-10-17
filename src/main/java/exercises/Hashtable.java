d9231aa2c583aaa183240648c676734b09c61d23
Philipp Haller <hallerp@gmail.com>
2019-10-17 11:04:06 +0200

    Beginning of hash table implementation
    So far, only includes the beginning of an insert method.

---
+++ src/main/java/exercises/Hashtable.java
@@ -0,0 +1,41 @@
+package exercises;
+
+class Entry {
+    Object key;
+    Object value;
+    Entry next;
+    Entry(Object key, Object value, Entry next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
+
+public class Hashtable {
+    private static final int SIZE = 1024;
+    private Entry[] table = new Entry[SIZE];
+
+    public void insert(Object key, Object value) {
+        if (key == null) {
+            throw new IllegalArgumentException("null key not permitted");
+        }
+        int hc = key.hashCode();
+        int index = hc % SIZE;
+        // now insert a new Entry object into
+        // the "table" array
+        Entry e = new Entry(key, value, null);
+        // best way: handle two cases separately
+        if (table[index] == null) {
+            // have not inserted anything yet
+            // at that index!
+            // all we need to do:
+            // create a chain with a single Entry
+            // object containing our key and value.
+            table[index] = e;
+        }
+    }
+
+    public Object lookup(Object key) {
+        return null;
+    }
+}
